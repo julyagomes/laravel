a2c07b332871f95e4dd3c414c6ccc7e878fe51f7
julyagomes <julyamodena01@gmail.com>
2023-05-10 13:38:21 -0300

    pedro

---
+++ resources/views/Site/home.blade.php
@@ -0,0 +1,14 @@
+@extends('site.layout')
+
+@section('title', 'Página Inicial')
+
+@section('content')
+
+    <div class =" container py -4">
+        <div class ="row">
+            <div class =" col sm -12">
+                <p class =" alert alert -info "> Lorem ipsum dolor sit amet . </p >
+            </div >
+        </div >
+    </div >
+@endsection
